8c88367e5948763aa40fb6a5a9fbceeb495de4c5
juan-leiva <juaeivamont@gmail.com>
2026-09-03 17:34:55 -0600

    autenticacion con correo y contrasena

--- app/src/main/java/com/example/servicios_autenticacion/InicioSesion.kt
+++ app/src/main/java/com/example/servicios_autenticacion/InicioSesion.kt
@@ -1,18 +1,47 @@
 package com.example.servicios_autenticacion
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.servicios_autenticacion.databinding.ActivityInicioSesionBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class InicioSesion : AppCompatActivity() {
     private lateinit var binding: ActivityInicioSesionBinding
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityInicioSesionBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.btnIniciar.setOnClickListener {
+            inciarSesion()
+        }
+    }
+
+    fun inciarSesion() {
+        val correo = binding.etCorreo.text.toString();
+        val contrasena = binding.etContrasena.text.toString()
+
+        auth.signInWithEmailAndPassword(correo, contrasena)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val inicio = Intent(this, MainActivity::class.java)
+                    startActivity(inicio)
+                    finish()
+                }
+            }.addOnFailureListener { task ->
+                Toast.makeText(applicationContext, task.localizedMessage, Toast.LENGTH_SHORT).show()
+            }
     }
 }
--- app/src/main/java/com/example/servicios_autenticacion/Registro.kt
+++ app/src/main/java/com/example/servicios_autenticacion/Registro.kt
@@ -1,18 +1,48 @@
 package com.example.servicios_autenticacion
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.servicios_autenticacion.databinding.ActivityRegistroBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Registro : AppCompatActivity() {
     private lateinit var binding: ActivityRegistroBinding
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        auth = FirebaseAuth.getInstance()
+
         binding = ActivityRegistroBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.btnRegistrar.setOnClickListener {
+            registrarse()
+        }
+
+    }
+
+    fun registrarse() {
+        val correo = binding.etCorreo.text.toString();
+        val contrasena = binding.etContrasena.text.toString()
+
+        auth.createUserWithEmailAndPassword(correo, contrasena)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val inicio = Intent(this, MainActivity::class.java)
+                    startActivity(inicio)
+                    finish()
+                }
+            }.addOnFailureListener { task ->
+                Toast.makeText(applicationContext, task.localizedMessage, Toast.LENGTH_SHORT).show()
+            }
     }
 }
